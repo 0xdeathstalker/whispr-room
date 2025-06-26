c005ca2a6deee6b3affb63b2de1625c75758a122
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-06-26 21:28:05 +0530

    add: shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
 export default function Home() {
   return (
     <div className="container mx-auto min-h-screen w-full max-w-[100ch] px-4">
-      <div className="pt-20 pb-6">
-        <h1 className="text-center font-sans">/whisprroom</h1>
+      <div className="mx-auto mt-20 flex h-96 w-full max-w-md flex-col items-center justify-between rounded-md border border-neutral-800 p-14">
+        <div>
+          <h1 className="mb-2 text-center font-mono">/whisprroom</h1>
 
-        <p className="text-center font-mono">a disposable chat room</p>
-      </div>
+          <p className="text-muted-foreground text-center font-sans">a disposable chat room</p>
+        </div>
 
-      <div className="mx-auto flex h-96 w-full max-w-md flex-col items-center justify-center rounded-md border border-neutral-800">
-        <input
-          className="mb-6 h-9 rounded-md border border-neutral-600 indent-2 placeholder:font-mono"
-          placeholder="enter username"
-        />
-        <div className="space-y-4 font-mono">
-          <button className="flex h-9 items-center justify-center rounded-md px-3 py-2 dark:bg-neutral-200 dark:text-neutral-900">
-            create room
-          </button>
-          <button className="flex h-9 items-center justify-center rounded-md border border-neutral-500 px-3 py-2 hover:bg-neutral-800">
-            join a room
-          </button>
+        <div className="flex w-full flex-col items-center">
+          <Input
+            placeholder="enter username..."
+            className="font-mono placeholder:font-mono"
+          />
+
+          <div className="mt-6 flex w-full flex-col items-center gap-4 font-mono">
+            <Button className="w-full">create room</Button>
+
+            <Separator />
+
+            <Button
+              variant="outline"
+              className="w-full"
+            >
+              join a room
+            </Button>
+          </div>
         </div>
       </div>
     </div>
