10a6b923f25e17a3bb37876a46a190b0ba060b5e
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-08-16 16:46:02 +0530

    update: remove copy button and added copy upon clicking the text

--- src/components/sections/chat/room-id.tsx
+++ src/components/sections/chat/room-id.tsx
@@ -1,22 +1,30 @@
 "use client";
 
-import CopyButton from "@/components/copy-button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { useScramble } from "use-scramble";
+import { useCopyToClipboard } from "usehooks-ts";
 
 export default function RoomId({ roomId }: { roomId: string }) {
+  const [, copy] = useCopyToClipboard();
+
   const { ref, replay } = useScramble({
     text: `#${roomId}`,
     speed: 0.4,
   });
 
   return (
-    <div className="flex items-center gap-1">
-      <h1
-        ref={ref}
-        onMouseOver={replay}
-        onFocus={replay}
-      />
-      <CopyButton textToCopy={roomId} />
-    </div>
+    <Tooltip>
+      <TooltipTrigger>
+        <div className="flex items-center gap-1" onClick={() => copy(roomId)}>
+          <h1 ref={ref} onMouseOver={replay} onFocus={replay} />
+          {/* <CopyButton textToCopy={roomId} /> */}
+        </div>
+      </TooltipTrigger>
+      <TooltipContent className="font-sans">click to copy</TooltipContent>
+    </Tooltip>
   );
 }
